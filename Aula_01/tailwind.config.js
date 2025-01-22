67edce1eb034dce5b2405648895719b08c29fde4
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2025-01-22 14:55:42 -0300

    feat: inicial course tailwind css box model

---
+++ Aula_01/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /* content: Configuração do endereço da pasta que sera aplicada os efeitos 
+  e os tipos de arquivos que serao reconhecidos.*/
+  content: ['./src/**/*.{html, js}'],  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
